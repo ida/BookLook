088874fa763977f1969620613cf9b70fcef8958f
Glitch (book-look) <none>
2018-01-27 17:36:46 +0000

    ⛳️📍 Updated with Glitch

--- .backup/development_helpers.js
+++ .backup/development_helpers.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
-  // Nightsight:
-  document.body.style = 'background: #000; color: #fff;'
+// Nightsight:
+document.body.style = 'background: #000; color: #fff;'
 
-  // Reload page every 7 seconds:
-  setInterval(function() { window.location.href = window.location.href }, 7777)
+// Reload page every 7 seconds:
+setInterval(function() { window.location.href = window.location.href }, 7000)
 
 
 });
---
+++ .backup/main.js
@@ -0,0 +1,18 @@
+// Wait until all element-objects are present:
+document.addEventListener('DOMContentLoaded', function(eve) {
+
+  
+  // Choose any element which content's you want to show in a BookLook:
+  var ele = document.getElementById('example-book-content')
+  
+
+  // Choose optional parameters, too:
+  var pagesPerBookWidth = null
+  var pageWidth = 333
+  var pageHeight = 444
+  
+  
+  // Execute:
+  BookLook(ele, pagesPerBookWidth, pageWidth, pageHeight)
+
+}); // DOM loaded
